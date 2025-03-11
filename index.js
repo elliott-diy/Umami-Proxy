bd10cc4d20d18ff7d91fd5a922435c1fc75e790d
Elliott <63378937+elliott-diy@users.noreply.github.com>
2025-03-11 12:35:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Happy developing ✨')
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const url = 'https://analytics.umami.is/script.js'
+  const response = await fetch(url, {
+    headers: {
+      'User-Agent': request.headers.get('User-Agent'), // Pass through the original User-Agent
+    },
+  })
+  
+  const newResponse = new Response(response.body, response)
+  newResponse.headers.set('Access-Control-Allow-Origin', '*') // Enable CORS
+  return newResponse
+}
